f7e12c6fbd1c7c7a5a29cb16ba00f5126c4d081e
Brandon <DeWittInTheRoad@gmail.com>
2018-02-21 08:42:28 -0500

    EmployeeDAO 95% complete

--- m2-w6d2-dao-exercises-pair/src/main/java/com/techelevator/projects/model/jdbc/JDBCEmployeeDAO.java
+++ m2-w6d2-dao-exercises-pair/src/main/java/com/techelevator/projects/model/jdbc/JDBCEmployeeDAO.java
@@ -21,34 +21,19 @@ public class JDBCEmployeeDAO implements EmployeeDAO {
 
 	@Override
 	public List<Employee> getAllEmployees() {
+			ArrayList<Employee> employees = new ArrayList<>();
+			Employee theEmployee = null;
+			String sqlgetAllEmployees = " SELECT department_id, first_name, last_name, birth_date, gender, hire_date" +
+					" FROM employee ";
+			SqlRowSet results = jdbcTemplate.queryForRowSet(sqlgetAllEmployees);
+			while(results.next()) {
+				theEmployee = mapRowToEmployee(results);
+				employees.add(theEmployee);
+			}
+			return employees;
 
-
-
-
-		return new ArrayList<>();
-
-	}
-	private Employee mapRowToEmployee(SqlRowSet results) {
-		Employee theEmployee;
-		theEmployee = new Employee();
-		theEmployee.setDepartmentId(results.getLong("department_id"));
-		theEmployee.setFirstName(results.getString("first_name"));
-		theEmployee.setLastName(results.getString("last_name"));
-		theEmployee.setBirthDay(results.getDate("birth_date").toLocalDate());
-		theEmployee.setGender(results.getString("gender").charAt(0));
-		theEmployee.setHireDate(results.getDate("hire_date").toLocalDate());
-
-		return theEmployee;
-
-	}
-	private long getNextEmployeeId() {
-		SqlRowSet nextIdResult= jdbcTemplate.queryForRowSet("SELECT nextval('seq_employee_id')");
-		if(nextIdResult.next()) {
-			return nextIdResult.getLong(1);
-		}else {
-			throw new RuntimeException("Something went wrong getting Id for new Employee");
 		}
-	}
+
 
 
 	@Override
@@ -68,21 +53,85 @@ public class JDBCEmployeeDAO implements EmployeeDAO {
 
 	@Override
 	public List<Employee> getEmployeesByDepartmentId(long id) {
-		return new ArrayList<>();
+		ArrayList<Employee> employees = new ArrayList<>();
+		Employee theEmployee = null;
+		String sqlsearchEmployeesByName = " SELECT department_id, first_name, last_name, birth_date, gender, hire_date" +
+				" FROM employee " + " WHERE first_name = ? " + " AND last_name = ?";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlsearchEmployeesByName, id);
+		while(results.next()) {
+			theEmployee = mapRowToEmployee(results);
+			employees.add(theEmployee);
+		}
+		return employees;
+
 	}
 
 	@Override
 	public List<Employee> getEmployeesWithoutProjects() {
-		return new ArrayList<>();
+		ArrayList<Employee> employees = new ArrayList<>();
+		Employee theEmployee = null;
+		String sqlgetEmployeesWithoutProjects = " SELECT department_id, first_name, last_name, birth_date, gender, hire_date" +
+				" FROM employee " + " FULL OUTER JOIN project_employee ON project_employee.employee_id = employee.employee_id " +
+				" FULL OUTER JOIN project ON project.project_id = project_employee.project_id " +
+				" WHERE project.project_id IS NULL ";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlgetEmployeesWithoutProjects);
+		while(results.next()) {
+			theEmployee = mapRowToEmployee(results);
+			employees.add(theEmployee);
+		}
+		return employees;
 	}
 
 	@Override
 	public List<Employee> getEmployeesByProjectId(Long projectId) {
-		return new ArrayList<>();
+		ArrayList<Employee> employees = new ArrayList<>();
+		Employee theEmployee = null;
+		String sqlgetEmployeesByProjectId = " SELECT department_id, first_name, last_name, birth_date, gender, hire_date" +
+				" FROM employee " + " FULL OUTER JOIN project_employee ON project_employee.employee_id = employee.employee_id " +
+				" FULL OUTER JOIN project ON project.project_id = project_employee.project_id " +
+				" WHERE project.project_id = ? ";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlgetEmployeesByProjectId, projectId);
+		while(results.next()) {
+			theEmployee = mapRowToEmployee(results);
+			employees.add(theEmployee);
+		}
+		return employees;
 	}
 
 	@Override
 	public void changeEmployeeDepartment(Long employeeId, Long departmentId) {
+			String sqlUpdateEmployeeDepartment = "UPDATE employee " +
+					" SET department_id " +
+					" WHERE employee_id = ? " +
+					" AND department_id = ? ";
+
+
+	jdbcTemplate.update(sqlUpdateEmployeeDepartment, employeeId, departmentId);
+		System.out.println("Employee: " + employeeId + " moved to department: " + departmentId);
+	}
+
+
+	private Employee mapRowToEmployee(SqlRowSet results) {
+		Employee theEmployee;
+		theEmployee = new Employee();
+		theEmployee.setDepartmentId(results.getLong("department_id"));
+		theEmployee.setFirstName(results.getString("first_name"));
+		theEmployee.setLastName(results.getString("last_name"));
+		theEmployee.setBirthDay(results.getDate("birth_date").toLocalDate());
+		theEmployee.setGender(results.getString("gender").charAt(0));
+		theEmployee.setHireDate(results.getDate("hire_date").toLocalDate());
+
+		return theEmployee;
 
 	}
+
+	private long getNextEmployeeId() {
+		SqlRowSet nextIdResult= jdbcTemplate.queryForRowSet("SELECT nextval('seq_employee_id')");
+		if(nextIdResult.next()) {
+			return nextIdResult.getLong(1);
+		}else {
+			throw new RuntimeException("Something went wrong getting Id for new Employee");
+		}
+	}
+
 }
