dd05471d3b1ab90ce1d65c5f52e4262f552329b1
Brandon <DeWittInTheRoad@gmail.com>
2018-02-22 14:52:21 -0500

    SiteDAO/JDBC

--- m2-capstone/src/main/java/com/techelevator/model/Site.java
+++ m2-capstone/src/main/java/com/techelevator/model/Site.java
@@ -1,44 +1,44 @@
 package com.techelevator.model;
 
 public class Site {
-    private long site_id;
-    private long campground_id;
-    private Integer site_number;
-    private Integer max_occupancy;
+    private long siteId;
+    private long campgroundId;
+    private Integer siteNumber;
+    private Integer maxOccupancy;
     private boolean accessible;
-    private Integer max_rv_length;
+    private Integer maxRVLength;
     private boolean utilities;
 
-    public long getSite_id() {
-        return site_id;
+    public long getSiteId() {
+        return siteId;
     }
 
-    public void setSite_id(long site_id) {
-        this.site_id = site_id;
+    public void setSiteId(long siteId) {
+        this.siteId = siteId;
     }
 
-    public long getCampground_id() {
-        return campground_id;
+    public long getCampgroundId() {
+        return campgroundId;
     }
 
-    public void setCampground_id(long campground_id) {
-        this.campground_id = campground_id;
+    public void setCampgroundId(long campgroundId) {
+        this.campgroundId = campgroundId;
     }
 
-    public long getSite_number() {
-        return site_number;
+    public Integer getSiteNumber() {
+        return siteNumber;
     }
 
-    public void setSite_number(Integer site_number) {
-        this.site_number = site_number;
+    public void setSiteNumber(Integer siteNumber) {
+        this.siteNumber = siteNumber;
     }
 
-    public long getMax_occupancy() {
-        return max_occupancy;
+    public Integer getMaxOccupancy() {
+        return maxOccupancy;
     }
 
-    public void setMax_occupancy(Integer max_occupancy) {
-        this.max_occupancy = max_occupancy;
+    public void setMaxOccupancy(Integer maxOccupancy) {
+        this.maxOccupancy = maxOccupancy;
     }
 
     public boolean isAccessible() {
@@ -49,12 +49,12 @@ public class Site {
         this.accessible = accessible;
     }
 
-    public long getMax_rv_length() {
-        return max_rv_length;
+    public Integer getMaxRVLength() {
+        return maxRVLength;
     }
 
-    public void setMax_rv_length(Integer max_rv_length) {
-        this.max_rv_length = max_rv_length;
+    public void setMaxRVLength(Integer maxRVLength) {
+        this.maxRVLength = maxRVLength;
     }
 
     public boolean isUtilities() {
--- m2-capstone/src/main/java/com/techelevator/model/jdbc/JDBCSiteDAO.java
+++ m2-capstone/src/main/java/com/techelevator/model/jdbc/JDBCSiteDAO.java
@@ -1,4 +1,64 @@
 package com.techelevator.model.jdbc;
 
-public class JDBCSiteDAO {
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import com.techelevator.model.Campground;
+import com.techelevator.model.Site;
+import com.techelevator.model.SiteDAO;
+
+    public class JDBCSiteDAO implements SiteDAO {
+
+        private JdbcTemplate jdbcTemplate;
+
+        public JDBCSiteDAO(DataSource dataSource) {
+            this.jdbcTemplate = new JdbcTemplate(dataSource);
+        }
+
+        public List<Site> getAvailableSites(Campground campground, LocalDate arrivalDate, LocalDate departureDate){
+            List<Site> availableSites = new ArrayList<>();
+
+
+            String sqlAvailableSites = "SELECT * FROM site WHERE campground_id = ? AND site_id NOT IN (SELECT site.site_id  "
+                    + "FROM reservation "
+                    + "JOIN site ON site.site_id = reservation.site_id "
+                    + "JOIN campground ON campground.campground_id = site.campground_id "
+                    + "WHERE site.campground_id = ? "
+                    + "AND ((?  BETWEEN reservation.from_date AND reservation.to_date)  "
+                    + " OR (?  BETWEEN reservation.from_date AND reservation.to_date) "
+                    + "OR (reservation.from_date BETWEEN ? AND ?) "
+                    + "OR (reservation.to_date BETWEEN ? AND ?))) "
+                    + " LIMIT 5; ";
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sqlAvailableSites, campground.getCampgroundId(), campground.getCampgroundId(),
+                    arrivalDate, departureDate, arrivalDate, departureDate,arrivalDate, departureDate) ;
+            while(results.next()) {
+                Site site = mapRowToSite(results);
+                availableSites.add(site);
+            }
+
+            return availableSites;
+        }
+
+        private Site mapRowToSite(SqlRowSet results) {
+            Site site;
+            site = new Site();
+            site.setSiteId(results.getLong("site_id"));
+            site.setCampgroundId(results.getLong("campground_id"));
+            site.setSiteNumber(results.getInt("site_number"));
+            site.setMaxOccupancy(results.getInt("max_occupancy"));
+            site.setAccessible(results.getBoolean("accessible"));
+            site.setMaxOccupancy(results.getInt("max_rv_length"));
+            site.setUtilities(results.getBoolean("utilities"));
+            return site;
+        }
+
+    }
+
 }
